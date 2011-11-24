eb75ffa7617996abb01ca09288039362561db197
aclin <r00944044@pclab78.pclab.csie.ntu.edu.tw>
2011-11-24 13:24:41 +0800

    Minor declarations for Facebook integration

--- src/com/alin/simplefacebook/SimpleFacebook.java
+++ src/com/alin/simplefacebook/SimpleFacebook.java
@@ -4,6 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class SimpleFacebook extends Activity {
+	
+	private static final String TAG = "SimpleFacebook";
+	private static final String APP_ID = "";
+	
+	private Facebook fb;
+	private AsyncFacebookRunner asyncRunner;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
